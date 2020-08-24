2839226588acfb8f541bd4aad281d55d3439b51a
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-08-24 16:27:47 -0300

    Supports confusion matrices for less than 19 classes

--- evaluate.py
+++ evaluate.py
@@ -13,9 +13,9 @@ def plot_confusion_matrix(model, datagen, repeat, columns):
             pred = model(x)
 
             if matrix is not None:
-              matrix += metrics.confusion_matrix(tf.argmax(y, axis = 1), tf.argmax(pred, axis = 1), list(range(19)))
+              matrix += metrics.confusion_matrix(tf.argmax(y, axis = 1), tf.argmax(pred, axis = 1), list(range(len(labels))))
             else:
-              matrix = metrics.confusion_matrix(tf.argmax(y, axis = 1), tf.argmax(pred, axis = 1), list(range(19)))
+              matrix = metrics.confusion_matrix(tf.argmax(y, axis = 1), tf.argmax(pred, axis = 1), list(range(len(labels))))
    
     df_matrix = pd.DataFrame((matrix) / (1e-6 + np.sum(matrix, axis = 0)), index = columns, columns = columns)
     sn.heatmap(df_matrix, vmin = 0, vmax = 1, annot = True)
